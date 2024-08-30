ce33d127864580cecfa3424a80376b6a1e112170
Kaif-ali-khan <154369130+Kaifali852@users.noreply.github.com>
2024-08-30 19:16:36 +0500

    todolist

---
+++ src/components/TodoInput.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const TodoInput = (props) => {
+  const [inputText, setInputText] = useState("");
+  const handleEnterPress = (e)=>{
+    if(e.keyCode===13){
+        props.addList(inputText)
+        setInputText("")
+    }
+  }  
+  return (
+    <div className="input-container">
+      <input
+        type="text"
+        className="input-box-todo"
+        placeholder="Enter your Task"
+        value={inputText}
+        onKeyDown={handleEnterPress}
+        onChange={(e) => {
+          setInputText(e.target.value);
+        }}
+      />
+      <button
+        className="add-btn"
+        onClick={() => {
+          props.addList(inputText);
+          setInputText("");
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default TodoInput;
